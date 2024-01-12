8ef478e72e7cbffa0598ccc30806a273ef28edbd
Парамонова Варвара <145575337+nasvayah@users.noreply.github.com>
2024-01-12 11:09:32 +0300

    Update fff.py

--- fff.py
+++ fff.py
@@ -1,13 +1,28 @@
+
 from spellchecker import SpellChecker
 import language_tool_python
 import re
+import emoji
 
-def check_errors(text):
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+                                u"\U00000000-\U00000009"
+                                u"\U0000000B-\U0000001F"
+                                u"\U00000080-\U00000400"
+                                u"\U00000402-\U0000040F"
+                                u"\U00000450-\U00000450"
+                                u"\U00000452-\U0010FFFF"
+                                "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
+
+def check_errors(text1):
+    text = deEmojify(text1)
+    if re.fullmatch(r'(\w){0,1}(\W){0,}', text):
+        return True
 
     tool = language_tool_python.LanguageTool('ru-RU')
 
     spell = SpellChecker(language='ru')
-
 
     custom_dictionary_file = "vocab.txt"
     with open(custom_dictionary_file, "r", encoding="utf-8") as file:
@@ -24,20 +39,14 @@ def check_errors(text):
     lowercased_text = ' '.join(word.lower() if not word[0].isupper() else word for word in text.split())
 
     words = [word for word in lowercased_text.split() if word.lower() not in ignored_words]
-    nonexistent_words_copy = spell.unknown(words)
-    grammar_errors = tool.check(lowercased_text)
 
-    text = re.sub(r'(?<=\w)([^\w\s]+)', '', text)
+
+    text = re.sub(r'(?<=\w)([^\w\s]+)', ' ', text)
 
     words = [word for word in text.split() if word.lower() not in ignored_words]
     nonexistent_words_copy = spell.unknown(words)
-    grammar_errors = tool.check(text)
+
     nonexistent_words = []
-    filtered_grammar_errors = []
-    for error in grammar_errors:
-        if not error.replacements: #or (len(str(error.replacements[0]).replace(str(error), '')) >2 or len(str(error).replace(str(error.replacements[0]), ''))>2):
-            continue
-        filtered_grammar_errors.append(error)
 
 
     for word in nonexistent_words_copy:
@@ -45,22 +54,69 @@ def check_errors(text):
             continue
         nonexistent_words.append(word)
 
-    return nonexistent_words, filtered_grammar_errors
+    text1 = text
 
-def main():
-    text = input("Введите текст для проверки: ")
+    return nonexistent_words
 
-    nonexistent_words, grammar_errors = check_errors(text)
+def result(text):
 
-    print("Несуществующие слова:")
-    for word in nonexistent_words:
-        print(word)
+    nonexistent_words= check_errors(text)
 
-    print("\nГрамматические ошибки:")
-    for error in grammar_errors:
-        print(error)
+    if nonexistent_words:
+        return True
+    return False
 
 
-main()
+
+
+import os.path
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DB:
+    def __init__(self):
+        self.conn = psycopg2.connect(host='ЗАМЕНИТЬ',
+                                     port='ЗАМЕНИТЬ',
+                                     user='ЗАМЕНИТЬ',
+                                     password='ЗАМЕНИТЬ',
+                                     dbname='ЗАМЕНИТЬ',
+                                     sslmode='require')
+        self.cur = self.conn.cursor()
+
+    def execute(self, query):
+        self.cur.execute(query)
+
+    def fetch_all(self):
+        return self.cur.fetchall()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+conn = DB()
+query = """select
+guides.id, guides.question_answer
+from guides
+where question_name in (
+    'answer_long_text_37961122'
+    ) 
+AND guides.question_answer notnull
+;"""
+conn.execute(query)
+data = conn.fetch_all()
+i = 0
+for row in data:
+    id = row[0]
+    text = row[1]
+    print(i)
+    i+=1
+    if result(text):
+        print(id, ' ', text)
+
+
+conn.close()
 
 
