2ea4682ca523ed1973c4d42f8a9ef0bf5b2e68c6
Парамонова Варвара <145575337+nasvayah@users.noreply.github.com>
2024-01-23 21:07:50 +0300

    Update fff.py

--- fff.py
+++ fff.py
@@ -17,7 +17,7 @@ def deEmojify(text):
 
 def check_errors(text1):
     text = deEmojify(text1)
-    if re.fullmatch(r'(\w){0,1}(\W){0,}', text):
+    if re.fullmatch(r'(\W){0,}', text):
         return True
 
 
@@ -31,8 +31,9 @@ def check_errors(text1):
 
     words = [word for word in lowercased_text.split() if word.lower() not in ignored_words]
 
-
-    text = re.sub(r'(?<=\w)([^\w\s]+)', ' ', text)
+    text = re.sub(r'([^\w\s]|\_)+', ' ', text)
+    text = re.sub(r'(\w)\1{2,}', r'\1\1', text)
+    text = re.sub(r'[a-zA-Z]', '', text)
 
     words = [word for word in text.split() if word.lower() not in ignored_words]
 
@@ -49,8 +50,6 @@ def result(text, custom_dictionary):
     if nonexistent_words:
         return True
     return False
-
-
 
 
 
@@ -84,31 +83,49 @@ class DB:
 
 conn = DB()
 query = """select
-guides.id, guides.question_answer
+guides.id, guides.question_id, guides.question_answer
 from guides
 where question_name in (
+    'answer_short_text_37961013',
+    'answer_short_text_39100929',
+    'answer_long_text_37961085',
+    'answer_short_text_39100908',
     'answer_long_text_37961122'
     ) 
-AND guides.question_answer notnull
+AND guides.question_answer notnull and guides.question_answer!='' and guides.question_answer!=' '
 ;"""
 conn.execute(query)
 data = conn.fetch_all()
 
+conn1 = DB()
+query1 = """select guides_info.fio from guides_info"""
+conn1.execute(query1)
+names = conn1.fetch_all()
+splnames = []
+for i in names:
+    name = str(i[0]).lower()
+    splnames = splnames + name.split()
 spell = SpellChecker(language='ru')
 
 custom_dictionary_file = "vocab.txt"
 with open(custom_dictionary_file, "r", encoding="utf-8") as file:
     custom_dictionary = set(
         word.strip() for word in file.readlines() if word.strip())
+
+
+custom_dictionary = custom_dictionary.union(set(splnames))
 spell.word_frequency.load_words(custom_dictionary)
 
-
-
+id_file = open("id_file.txt","w")
+question_id_file = open("question_id_file.txt", "w")
 for row in data:
     id = row[0]
-    text = row[1]
+    question_id = row[1]
+    text = row[2]
     if result(text, custom_dictionary):
-        print(id, ' ', text)
+        print(id, ' ',question_id, ' ', text)
+        id_file.write("'" + str(id) +"'" + ','+ '\n')
+        question_id_file.write("'" + str(question_id)+ "'"+ ',' + '\n')
 
 
 conn.close()
